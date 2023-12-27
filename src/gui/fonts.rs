0e8750fead9fb638598d051e3f65d9093857a76c
Julien Denozi <j-denozi@innopsys.fr>
2023-12-27 14:14:30 +0100

    Add font directory
    Signed-off-by: Julien Denozi <j-denozi@innopsys.fr>

--- src/gui/fonts.rs
+++ src/gui/fonts.rs
@@ -1,8 +1,6 @@
 pub(crate) fn setup_custom_fonts(ctx: &egui::Context) {
     let mut fonts = egui::FontDefinitions::default();
 
-    // Install my own font (maybe supporting non-latin characters).
-    // .ttf and .otf files supported.
     fonts.font_data.insert(
         "roboto".to_owned(),
         egui::FontData::from_static(include_bytes!(
